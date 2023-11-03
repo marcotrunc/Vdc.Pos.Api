75366c9751aedc6936f97d4c4cc28f2d4181c423
Marco Truncellito <Marco.Truncellito@eng.it>
2023-11-03 17:18:45 +0100

    Added category Model

---
+++ Vdc.Pos.Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vdc.Pos.Domain.Entities.Common;
+
+namespace Vdc.Pos.Domain.Entities
+{
+    public class Category : EntityBase<Guid>
+    {
+        public string Name { get; set; } = string.Empty;
+        public Guid? ParentId { get; set; } = null;
+        public Category? Parent { get; set; } = null;
+
+    }
+}
---
+++ Vdc.Pos.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vdc.Pos.Domain.Entities;
+using Vdc.Pos.Persistence.Configurations.Common;
+
+namespace Vdc.Pos.Persistence.Configurations
+{
+    public class CategoryConfiguration : EntityBaseConfiguration<Category, Guid>
+    {
+        public override void Configure(EntityTypeBuilder<Category> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(c => c.Name).IsRequired().HasMaxLength(50);
+
+            builder.HasOne(c => c.Parent)
+                .WithMany()
+                .HasForeignKey(c => c.ParentId)
+                .IsRequired(false);
+        }
+    }
+}
