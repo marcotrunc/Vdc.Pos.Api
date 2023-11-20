2c20c2077273bb98c66f661507728e6fbbaa538a
Marco Truncellito <Marco.Truncellito@eng.it>
2023-11-20 20:44:52 +0100

    Added variation option data transfer object

---
+++ Vdc.Pos.Domain/DTOs/Response/VariationOptionResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vdc.Pos.Domain.DTOs.Response
+{
+    public class VariationOptionResponse
+    {
+        public int Id { get; set; }
+        public int VariationId { get; set; }
+        public string Value { get; set; }
+    }
+}
